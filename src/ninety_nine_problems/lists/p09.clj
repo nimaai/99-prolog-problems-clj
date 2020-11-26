542a210900ee9dcdca4fe39ccb11643e7e6ef45b
Matus Kmit <matus.kmit@zhdk.ch>
2020-11-26 14:38:26 +0100

    use defne instead of letfn

--- src/ninety_nine_problems/lists/p09.clj
+++ src/ninety_nine_problems/lists/p09.clj
@@ -4,22 +4,24 @@
             [clojure.core.logic.fd :as lfd]
             [clojure.test :as t]))
 
+(l/defne transfero 
+  "ys is the list that remains from the list xs
+   when all leading copies of x are removed and transfered to z."
+  [a b c d]
+  ([x [] [] [x]])
+  ([x [y . ys] [y . ys] [x]] (l/!= x y))
+  ([x [x . xs] ys [x . zs]] (transfero x xs ys zs))))
+
 (t/with-test
   (defn packo
     "The list l2 is obtained from the list l1 by packing
      repeated occurrences of elements into separate sublists."
     [l1 l2]
-    (letfn [(transfero [a b c d]
-              (l/matche [a b c d]
-                ([x [] [] [x]])
-                ([x [y . ys] [y . ys] [x]] (l/!= x y))
-                ([x [x . xs] ys [x . zs]] (transfero x xs ys zs))))]
-      (l/matche [l1 l2]
-        ([[] []])
-        ([[x . xs] [z . zs]]
-         (l/fresh [ys]
-           (transfero x xs ys z)
-           (packo ys zs))))))
+    ([[] []])
+    ([[x . xs] [z . zs]]
+     (l/fresh [ys]
+       (transfero x xs ys z)
+       (packo ys zs))))
 
   (t/is (= (l/run* [q] (packo '[a,a,a,a,b,c,c,a,a,d,e,e,e,e] q))
            '([[a,a,a,a],[b],[c,c],[a,a],[d],[e,e,e,e]]))))
--- src/ninety_nine_problems/lists/p13.clj
+++ src/ninety_nine_problems/lists/p13.clj
@@ -4,30 +4,32 @@
             [clojure.core.logic.fd :as lfd]
             [clojure.test :as t]))
 
+(l/defne counto 
+  "ys is the list that remains from the list xs
+   when all leading copies of x are removed. t is the term [n,x],
+   where n is k plus the number of x's that can be removed from xs.
+   In the case of n=1, t is x, instead of the term [1,x]."
+  [a b c d e]
+  ([x [] [] 1 x])
+  ([x [] [] n [n x]] (la/> n 1))
+  ([x [y . ys] [y . ys] 1 x] (l/!= x y))
+  ([x [y . ys] [y . ys] n [n x]] (la/> n 1) (l/!= x y))
+  ([x [x . xs] ys k t] (l/fresh [k1]
+                         (l/is k1 k inc) 
+                         (counto x xs ys k1 t))))
 (t/with-test
-  (defn encode-directo
+  (l/defne encode-directo
     "The list l2 is obtained from the list l1 by 
      run-length encoding. Consecutive duplicates of elements are encoded 
      as terms [n,e], where n is the number of duplicates of the element e.
      However, if n equals 1 then the element is simply copied into the 
      output list."
     [l1 l2]
-    (letfn [(counto [a b c d e]
-              (l/matche [a b c d e]
-                ([x [] [] 1 x])
-                ([x [] [] n [n x]] (la/> n 1))
-                ([x [y . ys] [y . ys] 1 x] (l/!= x y))
-                ([x [y . ys] [y . ys] n [n x]] (la/> n 1) (l/!= x y))
-                ([x [x . xs] ys k t] (l/fresh [k1]
-                                       (l/is k1 k inc) 
-                                       (counto x xs ys k1 t)))))]
-      (l/matche [l1 l2]
-        ([[] []])
-        ([[x . xs] [z . zs]]
-         (l/fresh [ys]
-           (counto x xs ys 1 z)
-           (encode-directo ys zs))))))
-
+    ([[] []])
+    ([[x . xs] [z . zs]]
+     (l/fresh [ys]
+       (counto x xs ys 1 z)
+       (encode-directo ys zs))))
 
   (t/is (= (l/run* [q] (encode-directo '[a,a,a,a,b,c,c,a,a,d,e,e,e,e] q))
            '([[4,a],b,[2,c],[2,a],d,[4,e]]))))
